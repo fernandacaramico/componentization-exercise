5c4154c20a8a4abe0fcbf47d30a6ec88965ece59
fernandacaramico <fernandacaramico@gmail.com>
2021-04-05 16:42:06 -0300

    adiciona pagina html com css para componentizar

---
+++ components/src/App.js
@@ -0,0 +1,25 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
+          
+        </div>
+
+
+
+
+
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
